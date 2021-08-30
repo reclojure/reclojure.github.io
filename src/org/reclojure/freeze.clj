e60bdbd3766729d4fe8be8b8f64fca232469d1bf
Felipe Barros <felipebarros@protonmail.com>
2021-08-29 21:38:04 -0300

    reitit-jaatya setup
    This builds the HTML but with CSS injected at the header and still
    does not copy the public resources to the output directory.

--- src/org/reclojure/freeze.clj
+++ src/org/reclojure/freeze.clj
@@ -1,2 +1,12 @@
-(ns org.reclojure.freeze)
+(ns org.reclojure.freeze
+  (:require [org.reclojure.http :as http]
+            [lambdaisland.reitit-jaatya.freeze :as freeze]))
 
+(defn build [& args]
+  (freeze/iced (http/build-handler)))
+
+(comment
+  ;; this will create a `_site` folder in `reclojure`
+  ;; to browse it locally run
+  ;; cd _site && python3 -m http.server
+  (build))
