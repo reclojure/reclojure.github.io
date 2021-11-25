1410eea3c74fd88eb21c179e34d6c76efe7dcf2b
Felipe Barros <felipebarros@protonmail.com>
2021-11-25 10:10:20 +0000

    Workshops fixes and enhancements (#79)
    * Open event links and time.is links in a new tab
    
    Also ensure both don't wrap.
    
    * Fix the semantics of the workshop description
    
    And also remove the `workshop-event-link` element, which is not being
    used.
    
    * Improve semantics of the workshop libraries data
    
    Each cell represents a list of zero or more libraries.
    
    Also stop library names containing hyphens from wrapping and give the
    column a max-width that is the minimum viable value for the current
    content, at maximum size, to take at most two lines.
    
    * Rename workshop column `length` to `duration`
    
    Even though length isn't wrong but duration is, from what I could
    gather from a couple searches, way more common when referring to an
    event length.
    
    * Remove event-link helper fn and leverage utils in place
    
    The existing function `utils/external-link` allows for more attributes
    to be passed (like the class here) instead of just the link.
    
    This can probably be abstracted into a proper Ornament `defstyled` but
    it would require some refactoring so I'm leaving it for later.
    
    * Make the event link (now join) stand out from the other links
    
    I thought that moving it to the last column would be better because,
    as the website is in English (read left to right), the "registration"
    or "join" invitation fits more naturally at the end of a row of
    information.
    
    After trying it out though, I realized that the position at the
    beginning, maybe exactly because it inverts the assumed order, helps
    highlight the importance of the registration process, which is the
    main goal here.
    
    * Make workshop titles bold, add max-width to title and description
    
    As the table is already pretty long, the idea here is to create a
    certain degree of predictability for the vertical flow of the columns
    at its ideal horizontal size, ensuring _most_ titles look good, that
    description lines aren't too long and also to optimize white space
    between columns.
    
    * Centralize table heading of the Event column
    
    As I have transformed the aesthetics of the link to resemble a button
    by adding some padding to the links, without this setting it looks
    misaligned.
    
    * [WIP] Add base for toggling the description clamping on and off
    
    * Set the max-width of the workshops section to the max-content
    
    Now that the textual elements (which were responsible for pushing the
    view horizontally) have fixed max-widths, the section itself doesn't
    need a fixed max-width.
    
    * Add some icons
    
    * Add icons to the workshops section
    
    - The mobile version benefits from the icons as the table headers are
    not visible when scrolling beyond the example.
    - Also give the elements more room to improve the mobile experience.
    
    * Make our default gray a bit darker and remove green hue
    
    * Fix responsiveness for the workshops section

--- src/org/reclojure/views/components/workshops.clj
+++ src/org/reclojure/views/components/workshops.clj
@@ -4,7 +4,8 @@
             [garden.selectors :as gs]
             [org.reclojure.views.utils :as utils]
             [org.reclojure.views.-colors :as c]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [org.reclojure.views.assets :as assets]))
 
 (o/defstyled table :table
   {:border-collapse "collapse"})
@@ -13,22 +14,36 @@
   {:filter "grayscale(50%) opacity(50%)"
    :margin-bottom "2rem"
    :display "block"}
-  [:at-media {:min-width "60rem"}
+  [:at-media {:min-width "80rem"}
    {:display "revert"
     :filter "revert"}])
 
 (o/defstyled body :tbody
-  {}
-  [:a {:color "black"
+  {:color c/gray}
+  [:a {:color c/gray
        :text-decoration "underline 0.1em"}]
-  [:at-media {:min-width "60rem"}
+  [:at-media {:min-width "80rem"}
+   [:.event {:font-weight 700
+             :padding ".5rem 1rem"
+             :border-radius "2px"
+             :font-variant "all-small-caps"}]
    [:> [(gs/nth-child "4n+1")
         {:background-color c/lighter-blue
          :color c/copy-blue}
+        [:.event {:color c/copy-blue
+                  :font-weight 700
+                  :padding ".5rem 1rem"
+                  :border-radius "2px"
+                  :font-variant "all-small-caps"}]
         [:a {:color c/copy-blue}]]]
    [:> [(gs/nth-child "4n+3")
         {:background-color c/lighter-green
          :color c/darker-green}
+        [:.event {:color c/darker-green
+                  :font-weight 700
+                  :padding ".5rem 1rem"
+                  :border-radius "2px"
+                  :font-variant "all-small-caps"}]
         [:a {:color c/darker-green}]]]])
 
 (o/defstyled row :tr
@@ -37,20 +52,25 @@
    :text-align "left"
    :margin-bottom "2rem"
 
+   :color c/gray
+   "--icon-color" c/gray
    :box-shadow [["1rem" "1rem" 0 c/light-blue]]
    :border ["1px" "solid" c/light-blue]}
-  [:> [(gs/nth-child "2n+1") {:background-color c/lighter-blue}]]
+
+  [:> [(gs/nth-child "2n+1")
+       {:background-color c/lighter-blue}]]
   [:&:last-child {:margin-bottom "initial"}]
-  [:at-media {:min-width "60rem"}
+  [:at-media {:min-width "80rem"}
    {:display "revert"
     :box-shadow "revert"
     :border "revert"}
-   [:> [(gs/nth-child "2n+1") {:background-color "revert"}]]])
+   [:> [(gs/nth-child "2n+1")
+        {:background-color "revert"}]]])
 
 (def cell {:flex-basis "2rem"
            :line-height 1.4
            :padding-inline "1.5rem"
-           :padding-block ".5rem"
+           :padding-block "1rem"
            :vertical-align "baseline"})
 
 (o/defstyled th :th
@@ -58,56 +78,61 @@
   {:color c/gray
    :font-weight 400
    :font-style "italic"}
-  [:at-media {:min-width "60rem"}
+  [:at-media {:min-width "80rem"}
    {:line-height 2
     :font-style "revert"
-    :font-variant "all-small-caps"}])
+    :font-variant "all-small-caps"}
+   [:&:first-child {:text-align "center"}]])
 
 (o/defstyled td :td
   cell
   [:time {:font-style "italic"}])
 
-(o/defstyled a :a)
-
-(o/defstyled workshop-event-link :h3
-  {:font-weight   400
-   :margin        0
-   :padding-block "0.5rem"
-   :font-size     "1rem"})
+(o/defstyled libs :td
+  cell
+  [:ul {:max-width "20rem"
+        :list-style "none"
+        :padding 0
+        :margin 0}]
+  [:li {:display "inline"}])
 
 (o/defstyled workshop-title :h3
-  {:font-weight   400
-   :margin        0
+  {:margin        0
    :padding-block "0.5rem"
-   :font-size     "1rem"})
+   :font-size     "1rem"
+   :font-weight   700
+   :max-width     "21rem"})
 
-(o/defstyled workshop-description :h3
+(o/defstyled workshop-description :p
   {:font-weight   400
    :margin        0
    :padding-block "0.5rem"
-   :font-size     "1rem"})
+   :font-size     "1rem"
+   :max-width     "30rem"
+   :cursor        "pointer"}
+  ["&[aria-checked='on']" {:overflow-block "clip"
+                           :display "-webkit-box"
+                           :-webkit-line-clamp 1
+                           :-webkit-box-orient "vertical"
+                           :padding-block-end 0
+                           :margin-block-end ".5rem"}])
 
 (def month-names ["Jan" "Feb" "Mar" "Apr"
                   "May" "Jun" "Jul" "Aug"
                   "Sep" "Oct" "Nov" "Dec"])
 
-(defn event-link [link]
-  (when link
-    [a {:href link}
-     "Event link"]))
-
 (defn datetime-view [datetime]
   (if datetime
     (let [[year month day hour-minute] (string/split datetime #"-")
           month-name                   (-> month
-                         (Integer/parseInt)
-                         dec
-                         month-names)
+                                           (Integer/parseInt)
+                                           dec
+                                           month-names)
           [hour minute]                (string/split hour-minute #":")
           url                          (format "https://time.is/%s%s_%s_%s_%s_in_UTC"
-                      hour minute day month-name year)
+                                               hour minute day month-name year)
           caption (format "%s. %s, %s UTC" month-name day hour-minute)]
-      [a {:href url} caption])
+      [utils/external-link {:href url} [utils/nowrap caption]])
     [utils/nowrap "To be scheduled"]))
 
 (defn arrange-by-datetime [workshops]
@@ -128,10 +153,32 @@
                       :datetime
                       (or "a"))))))
 
+(o/defstyled iconify :span
+  {:display "inline-flex"
+   :align-items "baseline"}
+  [:svg {:height "1em"
+         :width "1em"
+         :flex-shrink 0}
+   [:path {:fill "var(--icon-color)"}]
+   [:&:first-child {:margin-inline-end "1em"}]
+   [(gs/& gs/last-child (gs/not gs/first-child)) {:margin-inline-start "0.5em"
+                                                  :display "none"}]]
+  [:at-supports {:gap "1em"}
+   {:gap "1em"}
+   [:svg
+    [:&:first-child {:margin-inline-end :unset}]
+    [(gs/& gs/last-child (gs/not gs/first-child))
+     {:margin-inline-start :unset}]]]
+  [:at-media {:min-width "80em"}
+   [:svg {:display "none"
+          :align-items "center"}
+    [(gs/& gs/last-child (gs/not gs/first-child)) {:display "block"}]]])
+
 (o/defstyled workshops :section
   ;; Section boilerplate to be abstracted
   {:margin ["13vh" "auto" "20vh"]
-   :width "min(82%, 76.75rem)"}
+   :max-width "max-content"
+   :padding "2rem"}
   [:.pre-title {:font-size "1.3rem"
                 :color c/gray
                 :font-variant "all-small-caps"
@@ -147,35 +194,39 @@
     [table
      [head
       [row
-       [th "Event"]
-       [th "Date"]
-       [th "Length"]
-       [th "Title"]
-       [th "Description"]
-       [th "Presenter"]
-       [th "Libraries"]]]
+       [th {:class "event"} [iconify assets/fa-up-right-from-square "Event" assets/fa-up-right-from-square]]
+       [th [iconify assets/fa-calendar-day "Date" assets/fa-up-right-from-square]]
+       [th [iconify assets/fa-stopwatch "Duration"]]
+       [th [iconify assets/fa-pen-nib "Title"]]
+       [th [iconify assets/fa-paragraph "Description"]]
+       [th [iconify assets/fa-chalkboard-user "Presenter"]]
+       [th [iconify assets/fa-book "Libraries" assets/fa-up-right-from-square]]]]
      [body
       (->> db/workshops
            arrange-by-datetime
            (map (fn [{:keys [link datetime length title description libraries presenter]}]
                   [row
-                   [td [event-link link]]
-                   [td {:class "datetime"} (datetime-view datetime)]
-                   [td {:class "length"} length]
-                   [td {:class "title"} [workshop-title title]]
-                   [td {:class "description"} [workshop-title description]]
-                   [td {:class "presenter"} [utils/nowrap presenter]]
-                   [td {:class "libs"}
-                    (let [lib-link (fn [{:keys [href name]}]
-                                     (utils/external-link {:href href} name))
-                          linked-libraries (map lib-link libraries)]
-                      (-> ", "
-                          repeat
-                          (interleave linked-libraries)
-                          rest))]])))]]]))
-
-
-
-
-
-
+                   [td [iconify assets/fa-up-right-from-square
+                        (when link
+                          [utils/external-link {:class "event"
+                                                :href link}
+                           [utils/nowrap "Join"]])]]
+                   [td {:class "datetime"} [iconify assets/fa-calendar-day (datetime-view datetime)]]
+                   [td {:class "duration"} [iconify assets/fa-stopwatch length]]
+                   [td {:class "title"} [iconify assets/fa-pen-nib [workshop-title title]]]
+                   [td {:class "description"}
+                    [iconify assets/fa-paragraph
+                     [workshop-description {:role "switch"
+                                            :aria-checked "off"
+                                            :tabindex 0} description]]]
+                   [td {:class "presenter"} [iconify assets/fa-chalkboard-user [utils/nowrap presenter]]]
+                   [libs
+                    [iconify assets/fa-book
+                     [:ul
+                      (let [lib-link (fn [{:keys [href name]}]
+                                       [:li [utils/external-link {:href href} [utils/nowrap name]]])
+                            linked-libraries (map lib-link libraries)]
+                        (-> ", "
+                            repeat
+                            (interleave linked-libraries)
+                            rest))]]]])))]]]))
