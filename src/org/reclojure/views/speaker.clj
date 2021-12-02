5fe4570873f55e628d9bb5b657e06de8ef6e5eef
Felipe Barros <felipebarros@protonmail.com>
2021-12-02 02:52:06 -0300

    Add abstract to speaker page, update design a bit
    Also add a placeholder to embed the video of the talk after the
    conference.

--- src/org/reclojure/views/speaker.clj
+++ src/org/reclojure/views/speaker.clj
@@ -2,91 +2,106 @@
   (:require [lambdaisland.ornament :refer [defstyled]]
             [org.reclojure.views.-colors :as c]
             [org.reclojure.views.-sizes :as s]
-            [org.reclojure.views.components.navigation :as navigation]))
+            [org.reclojure.views.components.navigation :as navigation]
+            [org.reclojure.db :as db]
+            [clojure.string :as str]))
 
-(defstyled speaker :article
+(defstyled main :main
+  {:max-width "min(82%, 60vmax)"
+   :margin "0 auto 5vmax"}
+  [:h1 :h2 {:color "#222"}]
+  [:p :small {:color "#333"}])
+
+(defstyled speaker :header
   {:display "flex"
    :flex-wrap "wrap"
-   :gap "1rem"
-   :align-items "flex-start"
+   :align-items "start"
 
-   :max-width "64rem"
-   :margin "5vmin auto"
-   :padding-left "2rem"
-   :padding-right "2rem"})
-
-(defstyled text :div
-  {:flex-basis 0
-   :flex-grow 999
-   :min-width "50%"}
-  [:h1 {:font-size "3rem"}]
-  [:p {:font-size "1.125rem"
-       :max-width "50ch"
-       :line-height 1.6}])
-
-(defstyled pic :img
-  {:aspect-ratio "1 / 1"
-   :object-fit "cover"
-   :height "20rem"
-   :width "100%"})
-
-(defstyled sidebar :aside
-  {:flex-basis "20rem"
+   :flex-basis "20rem"
    :flex-grow 1
-   :border ["1px" "solid" c/light-green]
-   :box-shadow [["1rem" "1rem" 0 c/light-green]]
+   :gap "4rem"
+   
+   :margin-top "5vmax"
+   ;; :border ["1px" "solid" c/light-green]
+   ;; :box-shadow [["1rem" "1rem" 0 c/light-green]]
    :margin-bottom "2rem"}
   [:img
    ["~ *" {:padding-left "1rem"
            :padding-right "1rem"}]
-   ["~ :last-child" {:margin-bottom "1.3rem"}]])
+   ["~ :last-child" {:margin-bottom "1.3rem"}]]
+  [:.info {:flex 1}]
+  [:h1 {:font-size "3rem"}
+   [:+ [:p {:font-size "1.125rem"
+            :line-height 1.6}]]])
 
-;; (defstyled speaker-card :li
-;;   {:background-color c/white
-;;    :box-shadow [["1rem 1rem " c/light-blue]]
-;;    :border ["1px" "solid" c/light-blue]}
+(defstyled pic :div
+  {:flex-basis "20rem"
+   :filter (str "drop-shadow(1rem 1rem " c/light-blue ")")}
+  [:img
+   {:aspect-ratio "1 / 1"
+    :object-fit "cover"
+    ;; :flex-basis "20rem"
+    :width "100%"
+    :max-height "20rem"
+    :clip-path "polygon(2% 6%,91% 0%,100% 85%,88% 100%,0% 100%)"}])
 
-;;   [:a {:color "initial"}]
+(defstyled abstract* :p
+  {:font-size "1.125rem"
+   :max-width "65ch"
+   :line-height 1.8})
 
-;;   ["> :last-child" {:padding-bottom 0}]
+(defstyled talk-title :h2
+  {:font-size "clamp(1.5rem, 4vw, 4rem)"
+   :margin-top "5vmax"
+   :margin-bottom "2vmax"})
 
-;;   [:img {:width "100%"
-;;          :object-fit "cover"
-;;          :aspect-ratio "1/1"
-;;          :max-height "max(10rem, 30vh)"}
-;;    ["~ *" {:margin-left "1rem"
-;;            :margin-right "1rem"}]]
+(defstyled by :p
+  {:font-size "1.25rem"})
 
-;;   [:h3 {:font-size "2rem"
-;;         :font-weight 700
-;;         :line-height 1
-;;         :margin-top "1.3rem"
-;;         :margin-bottom "1.3rem"}]
+(defstyled video-placeholder :div
+  {:aspect-ratio "16 / 9"
+   :background-color "hsl(0, 0%, 10%)"
+   :margin "2vmax auto"
+   :display "flex"
+   :align-items "center"
+   :justify-content "center"}
+  [:time {:color "white"
+          :font-size "3rem"
+          :line-height 1.5}
+   [:span {:display "block"}]])
 
-;;   [:p {:font-size "1rem"
-;;        :line-height 1.3
-;;        :margin-bottom "2rem"}]
-;;   ([speaker]
-;;    [:<>
-;;     [:a {:href (str "/2021/speaker/" (:slug speaker))}                     ;FIXME
-;;      [:img {:src (str "images/speakers/" (:picture speaker))
-;;             :alt (str "A picture of " (:name speaker) ".")
-;;             :width "300"
-;;             :height "300"}]
-;;      [:h3 (:name speaker)]
-;;      [:p (:brief speaker)]]]))
+(defn get-speaker [speaker speakers]
+  (seq (filter #{speaker} speakers)))
+
+(defn get-talks-by-author [speaker]
+  (let [full-schedule (into db/friday-2021 db/saturday-2021)]
+    (filter #(get-speaker speaker (:speakers %)) full-schedule)))
 
 (defn page [{:keys [name handle link picture description]}]
-  [:<>
-   [navigation/header
-    [navigation/navigation]]
-   [:main
-    [^:article speaker
-     [^:div text
-      [:header
-       [:h1 name]]
-      [:p description]]
-     [^:aside sidebar
-      [^:img pic {:alt (str "A picture of " name ".") :src (str "/images/speakers/" picture)}]
-      [:p (when handle (str "@" handle))]
-      [:p [:a {:href link} link]]]]]])
+  (let [talks (get-talks-by-author name)]
+    [:<>
+     [navigation/header
+      [navigation/navigation]]
+     [main
+      [speaker
+       [pic
+        [:img {:alt (str "A picture of " name ".")
+               :src (str "/images/speakers/" picture)}]]
+       [:div.info
+        [:h1 name]
+        [:p description]
+        [:p (when handle (str "@" handle))]
+        [:p [:a {:href link} link]]]]
+      (for [{:keys [title speakers abstract _time-start]} talks]
+        [:div
+         [talk-title title]
+         [by "By " (str/join " and " speakers)]
+         [video-placeholder
+          ;; [:time {:datetime time-start}
+          ;;  (case (subs time-start 8 10)
+          ;;    "03" [:span "Friday, 03/12/2021"
+          ;;          [:span (subs time-start 11 16) " UTC"]]
+          ;;    "04" [:span "Saturday, 04/12/2021"
+          ;;          [:span (subs time-start 11 16) " UTC"]])]
+          ]
+         [abstract* abstract]])]]))
