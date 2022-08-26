297f60b25654a8db10567c5b36971f844da55260
oxalorg <oxalorg@users.noreply.github.com>
2022-08-26 15:29:06 +0000

    deploy: 02b441425f033e9577083ddaeaa0b52dc3a2c5cc

---
+++ 2021/fix-links.sh
@@ -0,0 +1,6 @@
+sed -i "" "s/\/#speakers/\/2021\/#speakers/" **/*.html
+sed -i "" "s/\/#schedule/\/2021\/#schedule/" **/*.html
+sed -i "" "s/\/#workshops/\/2021\/#workshops/" **/*.html
+sed -i "" "s/\/#sponsors/\/2021\/#sponsors/" **/*.html
+sed -i "" "s/\/#keynote/\/2021\/#keynote/" **/*.html
+sed -i "" "s/\/#instructions/\/2021\/#instructions/" **/*.html
