8519af518a17d5d5204fd471a4debca7c03cde3f
Mitesh Shah <mitesh@miteshshah.com>
2022-11-07 06:48:31 +0530

    sessions section compatible with api
    the home page sessions section needs data in a particular structure,
    this commit adds proper transformations to the sessions data and
    the required changes to render the friday and saturday schedules on
    the home page
    
    the sessionize data doesn't send the type of sessions, so for now it's
    assuming that everything is a :Talk and it has the ability to hardcode
    panels and breaks using a map in db.clj which gets merged in with the
    sessions data from the API.

--- src/org/reclojure/db.clj
+++ src/org/reclojure/db.clj
@@ -1,5 +1,13 @@
 (ns org.reclojure.db
-  (:require [org.reclojure.sessionize :refer [speakers]]))
+  (:require [clojure.string :as str]
+            [org.reclojure.sessionize :refer [speakers sessions]]))
+
+(def friday "2022-12-02")
+(def saturday "2022-12-03")
+(def time-zone "+00:00")
+
+(defn make-datetime [day time]
+  (str day "T" time time-zone))
 
 (def speakers-data speakers)
 
@@ -14,39 +22,31 @@
       :link ""}]))
 
 (def schedule
-  {:friday
-   [{:index 1
-     :type :Talk
-     :time-start "10:00"
-     :time-end "10:25"
-     :duration 25
-     :title ""
-     :speakers [""]
-     :tags [:data-science]
-     :abstract ""}]
-   :saturday []})
+  (let [fri (partial make-datetime friday)
+        sat (partial make-datetime saturday)]
+    {:friday
+     [{:index 1
+       :type :Panel
+       :time-start (fri "10:00")
+       :time-end (fri "10:25")
+       :duration 25
+       :title ""
+       :speakers [""]
+       :tags [:data-science]
+       :abstract ""}]
+     :saturday []}))
 
-(def friday "2022-12-02")
-(def saturday "2022-12-03")
-(def time-zone "+00:00")
+(def friday-schedule
+  (->> @sessions
+       (filter #(str/starts-with? (:time-start %) friday))
+       (concat (:friday schedule))
+       (sort-by :time-start)))
 
-(defn make-datetime [time-zone date]
-  (fn [event]
-    (letfn [(datetime [time]
-              (str date "T" time time-zone))]
-      (-> event
-          (update :time-start datetime)
-          (update :time-end datetime)))))
-
-(def make-datetime-with-tz (partial make-datetime time-zone))
-
-(def friday-2021
-  (let [times->datetimes (make-datetime-with-tz friday)]
-    (map times->datetimes (:friday schedule))))
-
-(def saturday-2021
-  (let [times->datetimes (make-datetime-with-tz saturday)]
-    (map times->datetimes (:saturday schedule))))
+(def saturday-schedule
+  (->> @sessions
+       (filter #(str/starts-with? (:time-start %) saturday))
+       (concat (:friday schedule))
+       (sort-by :time-start)))
 
 (defn get-slug [name]
   (:slug (first (filter #(= name (:name %)) @speakers-data))))
--- src/org/reclojure/sessionize.clj
+++ src/org/reclojure/sessionize.clj
@@ -11,25 +11,6 @@
       :body
       (json/read-str :key-fn keyword)))
 
-(defn normalize-speakers [{:keys [speakers sessions]}]
-  (let [sessions-by-id (group-by :id sessions)]
-    (map
-     (fn [{:keys [sessions] :as speaker}]
-       (->> sessions
-            (map str)
-            (select-keys sessions-by-id)
-            vals
-            ;; group-by returns a vector of items, but session ids are unique
-            ;; so the vector only has one session
-            (map first)
-            (assoc speaker :sessions)))
-     speakers)))
-
-(defn normalize-sessions [{:keys [speakers sessions]}]
-  ;; we don't really havr the type of session info here
-  ;; is it a Talk, a Panel, or a Keynote?
-  sessions)
-
 (defn to-speaker-data [speaker]
   (transform/match
     speaker
@@ -55,13 +36,47 @@
   (transform/match
     session
     {:description ?description
+     :title ?title
      :startsAt ?starts-at
      :endsAt ?ends-at
      :speakers ?speakers}
     {:description ?description
-     :starts-at ?starts-at
-     :ends-at ?ends-at
+     :type :Talk
+     :title ?title
+     :time-start ?starts-at
+     :time-end ?ends-at
      :speakers ?speakers}))
+
+(defn normalize-speakers [{:keys [speakers sessions]}]
+  (let [sessions-by-id (group-by :id sessions)]
+    (map
+     (fn [{:keys [sessions] :as speaker}]
+       (->> sessions
+            (map str)
+            (select-keys sessions-by-id)
+            vals
+            ;; group-by returns a vector of items, but session ids are unique
+            ;; so the vector only has one session
+            (map first)
+            (assoc speaker :sessions)))
+     speakers)))
+
+(defn normalize-sessions [{:keys [speakers sessions]}]
+  ;; we don't really havr the type of session info here
+  ;; is it a Talk, a Panel, or a Keynote?
+  (let [speakers-by-id (group-by :id speakers)]
+    (map
+     (fn [{:keys [speakers] :as session}]
+       (->> speakers
+            (map str)
+            (select-keys speakers-by-id)
+            vals
+            ;; group-by returns a vector of items, but speaker ids are unique
+            ;; so the vector only has one session
+            (map first)
+            (map to-speaker-data)
+            (assoc session :speakers)))
+     sessions)))
 
 (def raw-api-data
   (->> (fetch-data! "https://sessionize.com/api/v2/3uz6wwyt/view/All"
