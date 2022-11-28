4902ec142eea645bd03cbf0490550254c0d34cc3
Felipe Barros <felipebarros@protonmail.com>
2022-11-28 18:08:25 +0100

    Fix Flexiana logo size

--- src/org/reclojure/views/home.clj
+++ src/org/reclojure/views/home.clj
@@ -139,8 +139,7 @@
   ; [:.healthunlocked {}]
   [:.freshcode {:max-height "13rem"
                 :height "100%"}]
-  [:.flexiana {:max-height "13rem"
-               :height "100%"}]
+  [:.flexiana {:height "100%"}]
   [:.gaiwan {:height "6rem"
              :width "100%"}]
   ; [:.hyde {:max-height "13rem"
