723c2c4b2a110a88e4cd3945514d14b27f59b4e3
weavejester <weavejester@users.noreply.github.com>
2024-11-10 21:21:25 +0000

    deploy: f3758e861e7b413f49e0590e9f246471d7f2a9d7

---
+++ ui.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navbar = document.getElementById('navbar');
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(
+        document.querySelectorAll('.navbar-burger'), 0);
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+
+            // Get the target from the "data-target" attribute
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
+
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
+
+            navbar.classList.toggle('is-burgered');
+        });
+    });
+
+    // Change opacity of navbar on scroll
+    const main   = document.getElementById('main');
+
+    let ticking = false;
+
+    document.getElementById('main').addEventListener('scroll', () => {
+        if (!ticking) {
+            window.requestAnimationFrame(() => {
+                const alpha = Math.min(main.scrollTop, 200) / 200.0;
+                navbar.style.setProperty('--reclojure-navbar-alpha', alpha);
+                ticking = false;
+            });
+            ticking = true;
+        }
+    });
+});
